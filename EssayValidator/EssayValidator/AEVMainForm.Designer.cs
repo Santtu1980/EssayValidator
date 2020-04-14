2ccc18074751a88a9d02b85c67a745c139aa4dda
Samuli <samuli.lahdenpera@ocuspecto.com>
2020-04-14 16:00:16 +0300

    Startup

---
+++ EssayValidator/EssayValidator/AEVMainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace EssayValidator
+{
+    partial class AEVMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Validate = new System.Windows.Forms.Button();
+            this.textBox_PathToEssayDocument = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Validate
+            // 
+            this.button_Validate.Location = new System.Drawing.Point(12, 68);
+            this.button_Validate.Name = "button_Validate";
+            this.button_Validate.Size = new System.Drawing.Size(75, 23);
+            this.button_Validate.TabIndex = 0;
+            this.button_Validate.Text = "Validate";
+            this.button_Validate.UseVisualStyleBackColor = true;
+            this.button_Validate.Click += new System.EventHandler(this.button_Validate_Click);
+            // 
+            // textBox_PathToEssayDocument
+            // 
+            this.textBox_PathToEssayDocument.Location = new System.Drawing.Point(12, 32);
+            this.textBox_PathToEssayDocument.Name = "textBox_PathToEssayDocument";
+            this.textBox_PathToEssayDocument.Size = new System.Drawing.Size(378, 20);
+            this.textBox_PathToEssayDocument.TabIndex = 1;
+            // 
+            // AEVMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_PathToEssayDocument);
+            this.Controls.Add(this.button_Validate);
+            this.Name = "AEVMainForm";
+            this.Text = "An Essay Validator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Validate;
+        private System.Windows.Forms.TextBox textBox_PathToEssayDocument;
+    }
+}
+
---
+++ EssayValidator/EssayValidator/AEVMainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using Application = Microsoft.Office.Interop.Word.Application;
+
+namespace EssayValidator
+{
+    public partial class AEVMainForm : Form
+    {
+        public AEVMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private string PathToFile = $@"C:\Temp";
+        private string FileName = "testi.docx";
+        private void button_Validate_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox_PathToEssayDocument.Text)) PathToFile = textBox_PathToEssayDocument.Text;
+
+            // TODO Path join
+            PathToFile += $@"\" + FileName;
+
+            if (!System.IO.File.Exists(PathToFile))
+            {
+                MessageBox.Show("Tiedostoa ei löydy.");
+                return;
+            }
+
+            var text = ReadFile(PathToFile);
+            List<string> sentences = text.Split('.').ToList<string>();
+            foreach (var sentenced in sentences)
+            {
+                MessageBox.Show(sentenced);
+            }
+        }
+
+        private string ReadFile(string documentPath)
+        {
+            Application word = new Application();
+            Document doc = new Document();
+
+            object fileName = documentPath;
+            // Define an object to pass to the API for missing parameters
+            object missing = System.Type.Missing;
+            doc = word.Documents.Open(ref fileName,
+                ref missing, ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing);
+
+            String read = string.Empty;
+            List<string> data = new List<string>();
+            foreach (Range tmpRange in doc.StoryRanges)
+            {
+                //read += tmpRange.Text + "<br>";
+                data.Add(tmpRange.Text);
+            }
+            ((_Document)doc).Close();
+            ((_Application)word).Quit();
+
+            return string.Join(" ", data);
+        }
+    }
+}
